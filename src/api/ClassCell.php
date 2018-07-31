637d62ab3104886d2aed3bf68c30ba0a7b69f214
fsrocha-dev <fsrocha.dev@gmail.com>
2018-07-31 00:12:55 -0300

    Add ClassCell.php

--- src/api/ClassCell.php
+++ src/api/ClassCell.php
@@ -2,5 +2,31 @@
 include("ClassConnect.php");
 
 class Cell extends ClassConnect{
+    public function showCell()
+    {
+        $FetchCell=$this->ConnectDB()->prepare("SELECT * FROM celulares");
+        $Fetch->execute();
 
+        $Arr=[];
+        $I=0;
+
+        while ($Fetch=$FetchCell->fetch(PDO::FETCH_ASSOC)) {
+            /*COLUNAS DO BANCO DE DADOS
+            id_cell
+            marca_cell
+            modelo_cell
+            valor_cell
+            */
+            $Arr[$I]=[
+                "id_cell"=>$Fetch['id_cell'],
+                "marca_cell"=>$Fetch['marca_cell'],
+                "modelo_cell"=>$Fetch['modelo_cell'],
+                "valor_cell"=>$Fetch['valor_cell'],
+            ];
+            $I++;
+        }
+        header("Access-Control-Allow-Origin:*");
+        header("Content-type: application/json");
+        echo json_encode($Arr);
+    }
 }
